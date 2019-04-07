fce9ed81e144263d69fd8673326f5fcd59d3e769
onemira <mirabulgogi@gmail.com>
2019-04-07 17:15:15 -0400

    crud works

--- db/migrate/20190404152852_create_users.rb
+++ db/migrate/20190404152852_create_users.rb
@@ -6,6 +6,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.text :auth_sub
 
       t.timestamps
-    end
+    end"
   end
 end
--- db/migrate/20190404215853_remove_name_from_image.rb
+++ db/migrate/20190404215853_remove_name_from_image.rb
@@ -1,7 +1,5 @@
 class RemoveNameFromImage < ActiveRecord::Migration[5.2]
   def change
-    remove_column :images, :name, :string
-    remove_column :videos, :name, :string
-    remove_column :links, :name, :string
+    remove_column :images, :name
   end
 end
