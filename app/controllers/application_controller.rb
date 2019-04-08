0a2220b568aee677f25d42827fbfffad8c63f764
onemira <mirabulgogi@gmail.com>
2019-04-08 13:44:42 -0400

    Add the JWT support code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
-class ApplicationController < ActionController::API
-end
+private
+  def current_user
+    @user ||= begin
+      token = request.headers["Authorization"].to_s.split(" ").last
+      payload, header = *JSONWebToken.verify(token)
+      User.from_auth_hash(payload)
+    end
+  rescue JWT::VerificationError, JWT::DecodeError
+    nil
+  end
+
