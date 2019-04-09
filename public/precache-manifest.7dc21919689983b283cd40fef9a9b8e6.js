05127f770a0f14cc469fbf81c985604b1e888a3f
onemira <mirabulgogi@gmail.com>
2019-04-09 15:53:42 -0400

    button updated

---
+++ public/precache-manifest.7dc21919689983b283cd40fef9a9b8e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "34762fa6cca10e5984e6b16d30e130a4",
+    "url": "/static/media/simple.34762fa6.jpg"
+  },
+  {
+    "revision": "5c4d0e6e506953f3593362e6d17df2a9",
+    "url": "/static/media/data.5c4d0e6e.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5675e152b353f598fd1",
+    "url": "/static/js/main.04b160a9.chunk.js"
+  },
+  {
+    "revision": "a6e076f21d54e328e8ff",
+    "url": "/static/js/2.1a08cd05.chunk.js"
+  },
+  {
+    "revision": "a5675e152b353f598fd1",
+    "url": "/static/css/main.e87cfb22.chunk.css"
+  },
+  {
+    "revision": "2dc91b116acaaefc3a165a454ebcb884",
+    "url": "/index.html"
+  }
+];
